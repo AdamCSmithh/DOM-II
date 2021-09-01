336e3049c8540282e038ebb419a3d239b5b226fa
acshatr <46507461+acshatr@users.noreply.github.com>
2021-08-31 22:11:10 -0700

    Finished

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const buttons = document.querySelector('.btn');
+const nav = document.querySelector('.nav');
+const navs = nav.querySelector('.nav-link');
+
+navs.addEventListener('onclick', e => {
+    console.log(e);
+    }
+);
+
+console.log('apples');
